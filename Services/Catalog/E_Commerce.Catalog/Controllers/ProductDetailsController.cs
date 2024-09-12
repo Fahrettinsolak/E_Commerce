66631bd3edc98c3262f918e6174bcd96ee495672
Fahrettin Solak <fhrttnsolak96@gmail.com>
2024-09-12 19:43:42 +0300

    Project Update

--- Services/Catalog/E_Commerce.Catalog/Controllers/ProductDetailsController.cs
+++ Services/Catalog/E_Commerce.Catalog/Controllers/ProductDetailsController.cs
@@ -9,40 +9,40 @@ namespace E_Commerce.Catalog.Controllers
     [ApiController]
     public class ProductDetailsController : ControllerBase
     {
-        private readonly IProductDetailService _ProductDetailService;
+        private readonly IProductDetailService _productDetailService;
 
         public ProductDetailsController(IProductDetailService ProductDetailService)
         {
-            _ProductDetailService = ProductDetailService;
+            _productDetailService = ProductDetailService;
         }
         [HttpGet]
         public async Task<IActionResult> ProductDetailList()
         {
-            var values = await _ProductDetailService.GetAllProductDetailAsync();
+            var values = await _productDetailService.GetAllProductDetailAsync();
             return Ok(values);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProductDetailById(string id)
         {
-            var value = await _ProductDetailService.GetByIdProductDetailAsync(id);
+            var value = await _productDetailService.GetByIdProductDetailAsync(id);
             return Ok(value);
         }
         [HttpPost]
         public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
         {
-            await _ProductDetailService.CreateProductDetailDtoAsync(createProductDetailDto);
+            await _productDetailService.CreateProductDetailDtoAsync(createProductDetailDto);
             return Ok("Ürün Detayı başarıyla eklendi");
         }
         [HttpDelete]
         public async Task<IActionResult> DeleteProductDetail(string id)
         {
-            await _ProductDetailService.DeleteProductDetailDtoAsync(id);
+            await _productDetailService.DeleteProductDetailDtoAsync(id);
             return Ok("Ürün Detayı başarıyla silindi");
         }
         [HttpPut]
         public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
         {
-            await _ProductDetailService.UpdateProductDetailDtoAsync(updateProductDetailDto);
+            await _productDetailService.UpdateProductDetailDtoAsync(updateProductDetailDto);
             return Ok("Ürün Detayı başarıyla güncellendi");
         }
     }
--- Services/Catalog/E_Commerce.Catalog/Controllers/ProductImagesController.cs
+++ Services/Catalog/E_Commerce.Catalog/Controllers/ProductImagesController.cs
@@ -9,40 +9,40 @@ namespace E_Commerce.Catalog.Controllers
     [ApiController]
     public class ProductImagesController : ControllerBase
     {
-        private readonly IProductImageService _ProductImageService;
+        private readonly IProductImageService _productImageService;
 
         public ProductImagesController(IProductImageService ProductImageService)
         {
-            _ProductImageService = ProductImageService;
+            _productImageService = ProductImageService;
         }
         [HttpGet]
         public async Task<IActionResult> ProductImageList()
         {
-            var values = await _ProductImageService.GetAllProductImageAsync();
+            var values = await _productImageService.GetAllProductImageAsync();
             return Ok(values);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProductImageById(string id)
         {
-            var value = await _ProductImageService.GetByIdProductImageAsync(id);
+            var value = await _productImageService.GetByIdProductImageAsync(id);
             return Ok(value);
         }
         [HttpPost]
         public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
         {
-            await _ProductImageService.CreateProductImageDtoAsync(createProductImageDto);
+            await _productImageService.CreateProductImageDtoAsync(createProductImageDto);
             return Ok("Ürün Görselleri başarıyla eklendi");
         }
         [HttpDelete]
         public async Task<IActionResult> DeleteProductImage(string id)
         {
-            await _ProductImageService.DeleteProductImageDtoAsync(id);
+            await _productImageService.DeleteProductImageDtoAsync(id);
             return Ok("Ürün Görselleri başarıyla silindi");
         }
         [HttpPut]
         public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
         {
-            await _ProductImageService.UpdateProductImageDtoAsync(updateProductImageDto);
+            await _productImageService.UpdateProductImageDtoAsync(updateProductImageDto);
             return Ok("Ürün Görselleri başarıyla güncellendi");
         }
     }

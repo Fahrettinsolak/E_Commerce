5e834655640512903607142c962fd2cc1d554b20
Fahrettin Solak <fhrttnsolak96@gmail.com>
2024-09-13 17:36:42 +0300

    Update Projected

---
+++ Services/Order/Core/E_Commerce.Order.Application/Features/Mediator/Handlers/OrderingHandlers/GetOrderingQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using E_Commerce.Order.Application.Features.CQRS.Results.AdressResults;
+using E_Commerce.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using E_Commerce.Order.Application.Features.Mediator.Results.OrderingResults;
+using E_Commerce.Order.Application.Interfaces;
+using E_Commerce.Order.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingQueryHandler : IRequestHandler<GetOrderingQuery, List<GetOrderingQueryResult>>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public GetOrderingQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetOrderingQueryResult>> Handle(GetOrderingQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetOrderingQueryResult
+            {
+                OrderingId = x.OrderingId,
+                UserId = x.UserId,
+                TotalPrice = x.TotalPrice,
+                OrderDate = x.OrderDate
+            }).ToList();
+        }
+    }
+}
